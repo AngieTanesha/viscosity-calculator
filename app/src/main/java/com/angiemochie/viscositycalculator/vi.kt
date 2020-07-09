90dd937a4d886156f8e4364512a12fd7871d6ae0
AngieTanesha <48684061+AngieTanesha@users.noreply.github.com>
2020-07-10 00:50:46 +1000

    Without using Krangl

--- app/src/main/java/com/angiemochie/viscositycalculator/vi.kt
+++ app/src/main/java/com/angiemochie/viscositycalculator/vi.kt
@@ -6,11 +6,10 @@ import android.text.Editable
 import android.text.TextWatcher
 import android.widget.EditText
 import android.widget.TextView
-import krangl.dataFrameOf
-import krangl.ge
-import krangl.lt
+
 import kotlin.math.ln
 import kotlin.math.pow
+import kotlin.Double as Double
 
 
 class vi : AppCompatActivity(), TextWatcher {
@@ -37,47 +36,44 @@ class vi : AppCompatActivity(), TextWatcher {
     }
 
     private fun calculateVi(){
-        val v40 : Double = vi_40_EditText.text.toString().toDouble()
-        val v100 : Double = vi_100_EditText.text.toString().toDouble()
-        val VI : Double
 
-        val df = dataFrameOf("Ymin","Ymax","a","b","c","d","e","f")(
-            2.0,3.8,1.14673,1.7576,-0.109,0.84155,1.5521,-0.077,
-            3.8,4.4,3.38095,-15.4952,33.196,0.78571,1.7929,-0.183,
-            4.4,5.0,2.5000,-7.2143,13.812,0.82143,1.5679,0.119,
-            5.0,6.4,0.10100,16.6350,-45.469,0.04985,9.1613,-18.557,
-            6.4,7.0,3.35714,-23.5643,78.466,0.22619,7.7369,-16.656,
-            7.0,7.7,0.01191,21.4750,-72.870,0.79762,-0.7321,14.610,
-            7.7,9.0,0.41858,16.1558,-56.040,0.05794,10.5156,-28.240,
-            9.0,12.0,0.88779,7.5527,-16.600,0.26665,6.7015,-10.810,
-            12.0,15.0,0.76720,10.7972,-38.180,0.20073,8.4658,-22.490,
-            15.0,18.0,0.97305,5.3135,-2.200,0.28889,5.9741,-4.930,
-            18.0,22.0,0.97256,5.2500,-0.980,0.24504,7.4160,-16.730,
-            22.0,28.0,0.91413,7.4759,-21.820,0.20323,9.1267,-34.230,
-            28.0,40.0,0.87031,9.7157,-50.770,0.18411,10.1015,-46.750,
-            40.0,55.0,0.84703,12.6752,-133.310,0.17029,11.4866,-80.620,
-            55.0,70.0,0.85921,11.1009,-83.19,0.17130,11.3680,-76.940,
-            70.0,71.0,0.83531,14.6731,-216.246,0.16841,11.8493,-96.94)
+        val v40 = vi_40_EditText.text.toString().toDouble()
+        val v100 = vi_100_EditText.text.toString().toDouble()
+        var viscosityIndex : Double
 
-        if (v100 in 2.0..71.0){
-            val eqn = df.filter{ it["Ymin"] lt v100 }.filter{ it["Ymax"] ge v100}
+        // Initiate the lookup table
+        val yMin : List<Double> = listOf(2.0, 3.8, 4.4, 5.0, 6.4, 7.0, 7.7, 9.0, 12.0,
+            15.0, 18.0, 22.0, 28.0, 40.0, 55.0, 70.0)
+        val yMax : List<Double> = listOf(3.8, 4.4, 5.0, 6.4, 7.0, 7.7, 9.0,12.0,
+            15.0, 18.0, 22.0, 28.0, 40.0, 55.0, 70.0, 71.0)
+        val yValues = yMin zip yMax
+        val a = listOf(1.14673,3.38095,2.5000,0.10100,3.35714,0.01191,0.41858,0.88779,0.76720,0.97305,0.97256,0.91413,0.87031,0.84703,0.85921,0.83531)
+        val b = listOf(1.7576,-15.4952,-7.2143,16.6350,-23.5643,21.4750,16.1558,7.5527,10.7972,5.3135,5.2500,7.4759,9.7157,12.6752,11.1009,14.6731)
+        val c = listOf(-0.109,33.196,13.812,-45.469,78.466,-72.870,-56.040,-16.600,-38.180,-2.200,-0.980,-21.820,-50.770,-133.310,-83.19,-216.246)
+        val d = listOf(0.84155,0.78571,0.82143,0.04985,0.22619,0.79762,0.05794,0.26665,0.20073,0.28889,0.24504,0.20323,0.18411,0.17029,0.17130,0.16841)
+        val e = listOf(1.5521,1.7929,1.5679,9.1613,7.7369,-0.7321,10.5156,6.7015,8.4658,5.9741,7.4160,9.1267,10.1015,11.4866,11.3680,11.8493)
+        val f = listOf(-0.077,-0.183,0.119,-18.557,-16.656,14.610,-28.240,-10.810,-22.490,-4.930,-16.730,-34.230,-46.750,-80.620,-76.940,-96.947)
 
-            val L : Double = eqn["a"][0].toString().toDouble().times(v100.pow(2)) + eqn["b"][0].toString().toDouble().times(v100) + eqn["c"][0].toString().toDouble()
-            val H : Double = eqn["d"][0].toString().toDouble().times(v100.pow(2)) + eqn["e"][0].toString().toDouble().times(v100) + eqn["f"][0].toString().toDouble()
+        if (2.0 <= v100 && v100 <= 71.0){
+            val yFiltered = yValues.filter{ it.first <= v100 && it.second > v100}
+            val yIndex = yValues.indexOf(yFiltered[0])
 
-            if (v40 <= H){
-                VI = ((L - v40)/(L - H)).times(v100)
+            val L : Double = a[yIndex].times(v100.pow(2)) + b[yIndex].times(v100) + c[yIndex]
+            val H : Double = d[yIndex].times(v100.pow(2)) + e[yIndex].times(v100) + f[yIndex]
+
+
+            if (v40 >= H){
+                viscosityIndex = ((L - v40)/(L - H)).times(100.0)
             } else {
                 val N : Double = (ln(H)- ln(v40))/ln(v100)
-                VI  = (10.0.pow(N) - 1).div(0.00715).plus(100)
+                viscosityIndex  = (10.0.pow(N) - 1).div(0.00715).plus(100)
             }
 
-            viAnswerStaticTextView.text = String.format("%.2f", VI)
+            viAnswerStaticTextView.text = String.format("%.2f", viscosityIndex)
+        } else {
 
+            viAnswerStaticTextView.text = "Invalid Inputs"
         }
-
-        viAnswerStaticTextView.text = String.format("Invalid inputs")
-
         
     }
 
